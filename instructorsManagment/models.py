0e43cf528b9f829cb2ccd57ffaf3e81fb9379d1c
Ghada Alnaqi <ghnaqi@Ghadas-MacBook-Pro.local>
2014-12-01 12:40:00 +0300

    Added models.py with all related stuff

--- instructorsManagment/models.py
+++ instructorsManagment/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Instructor(models.Model):
+    inst = models.OneToOneField(User)
+    mobile = models.BigIntegerField(max_length=8)
+    membership = models.BigIntegerField()
+
+    def __unicode__(self):
+        return u"{}".format(self.inst.get_full_name())
