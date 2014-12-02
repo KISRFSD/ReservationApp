be929d926a743edf531e9afe4792948b0b3175ca
Musab Alshatti <m93b85@gmail.com>
2014-12-02 11:39:18 +0300

    add foreign key relation to instructor in eventManage

--- instructorsManagment/models.py
+++ instructorsManagment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class Instructor(models.Model):
     inst = models.OneToOneField(User)
     mobile = models.BigIntegerField(max_length=8)
