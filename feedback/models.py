3f04630e67ecc1f2fc0641375f6832c4e6e29ef0
Nasser Bader Hussain <nasser.bader@outlook.com>
2014-12-02 11:41:56 +0300

    finish feedback application

--- feedback/models.py
+++ feedback/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Feedback(models.Model):
-    feedDate = models.DateField()
+    feedDate = models.DateField(auto_now_add=True)
     name = models.CharField(max_length=30)
     email = models.EmailField()
     mobile = models.IntegerField()
--- reservations/urls.py
+++ reservations/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'reservations.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^feedback/$', 'feedback.views.feed', name='feedback'),
     url(r'^admin/', include(admin.site.urls)),
 )
