340183c9394cc75829948ec78992ef6d6479ab06
Ghada Alnaqi <ghnaqi@gmail.com>
2014-12-03 06:32:48 +0300

    phone number validation added

--- instructorsManagment/models.py
+++ instructorsManagment/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 # Create your models here.
 
 class Instructor(models.Model):
     inst = models.OneToOneField(User)
-    mobile = models.BigIntegerField(max_length=8)
+    mobile = models.BigIntegerField(max_length=8,
+                                    validators=[
+                                        RegexValidator(
+                                            regex='[2]\d{7}|[5]\d{7}|[6]\d{7}|[9]\d{7}',
+                                            message='Invalid mobile number'),])
     membership = models.BigIntegerField()
 
     def __unicode__(self):
